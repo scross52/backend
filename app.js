948373609dc95b8e099bfdd7712870d20fb598a8
scross52 <scross52@ivytech.edu>
2025-10-06 19:32:26 -0400

    Create database functionality

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 const router = express.Router()
 
-//grab all the songs in a database
+// Get list of all songs in the database
 router.get("/songs", async(req,res) => {
   try{
     const songs = await Song.find({})
@@ -22,6 +22,18 @@ router.get("/songs", async(req,res) => {
   }
 })
 
+//Grab a single song in the database
+router.get("/songs/:id", async(req,res) => {
+  try {
+    const song = await Song.findById(req.params.id)
+    res.json(song)
+  } catch (err) {
+    res.status(400).send(err)
+
+  }
+})
+
+//added a song to the database
 router.post("/songs", async(req,res) => {
   try {
     const song = await new Song(req.body)
@@ -29,6 +41,22 @@ router.post("/songs", async(req,res) => {
     res.status(201).json(song)
     console.log(song)
   } catch (err) {
+    res.status(400).send(err)
+  }
+})
+
+//update is to update an existing record/resource/database entry...it uses a put request
+router.put("/songs/:id", async(req,res) => {
+  //first we need to find and update the song the front end wants us to update.
+  //to do this we need to request the id of the song from request
+  //and then find it in the database and update it
+  try{
+    const song = req.body
+    await Song.updateOne({_id: req.params.id}, song)
+    console.log(song)
+    res.sendStatus(204)
+
+  }catch (err) {
     res.status(400).send(err)
   }
 })
--- models/songs.js
+++ models/songs.js
@@ -1,6 +1,7 @@
 const db = require("../db")
 
 const Song = db.model("Song", {
+  //hidden property _id
   title: {type:String, required:true},
   artist: String,
   popularity: {type:Number, min:1, max:10},
